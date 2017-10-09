0735f2db79fea8026abf1ccf29f7bc6f2c8dfaee
evonox <prehnal@gmail.com>
2017-10-10 00:29:09 +0200

    Publish script on PowerShell

--- publish.ps1
+++ publish.ps1
@@ -1,11 +1,23 @@
+If($args.Count -eq 0) {
+    Write-Output "Enter the archive version."
+    Exit
+}
+
 Remove-Item "..\release" -Recurse -ErrorAction Ignore -Force
 mkdir "..\Release"
 Copy-Item -Destination "..\Release\" -Recurse ".\"
-chdir "..\Release"
-# Remove-Item ".\.git" -Recurse -ErrorAction Ignore -Force
+chdir "..\Release\jdoodle-for-wp"
+Remove-Item ".\.git" -Recurse -ErrorAction Ignore -Force
+Remove-Item ".\.gitignore"
+Remove-Item ".\publish.ps1"
+Remove-Item ".\README.md"
+Remove-Item ".\update.bat"
+chdir ".."
+$filename = "..\jdoodle-for-wp-" + $args[0] + ".zip"
+Compress-Archive -Path ".\jdoodle-for-wp" -DestinationPath $filename
+chdir "..\jdoodle-for-wp"
+Remove-Item "..\release" -Recurse -ErrorAction Ignore -Force
 
-
-# Set-Location "..\jdoodle-for-wp"
 
 
 
